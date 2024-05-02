31460b28262f3214944f22e20a836e1a4f76ddf2
Noah Carrier <67935880+noahcarrier@users.noreply.github.com>
2024-05-02 12:10:06 -0400

    new navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,14 +3,14 @@ import React from 'react';
 export default function Navbar() {
     return (
         <nav className='p-4'>
-            <div className="container mx-auto flex justify-between items-center">
+            <div className="container mx-auto flex justify-center items-center">
                 <span className="text-white text-lg font-bold"></span>
                 
                 <div className="hidden md:flex items-center">
-                    <a href="#" className="mr-16 navitem" >About</a>
-                    <a href="#" className="mr-16 navitem" >Skills</a>
-                    <a href="#" className="mr-16 navitem" >Projects</a>
-                    <a href="#" className="navitem">Contact</a>
+                    <a href="#about" className="mr-16 navitem hover:underline" >About</a>
+                    <a href="#skills" className="mr-16 navitem hover:underline" >Skills</a>
+                    <a href="#projects" className="mr-16 navitem hover:underline" >Projects</a>
+                    <a href="#contact" className="navitem hover:underline">Contact</a>
                 </div>
             </div>
         </nav>
